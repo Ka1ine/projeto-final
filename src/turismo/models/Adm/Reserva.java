1d2e7b4ab6c211fb167c6bf46dcfb77bbaab941b
Jpnovaes15 <j174494@dac.unicamp.br>
2023-11-08 18:26:02 -0300

    Criar Reservas

---
+++ src/turismo/models/Adm/Reserva.java
@@ -0,0 +1,13 @@
+package turismo.models.Adm;
+
+import java.time.LocalDate;
+
+import turismo.models.Pacotes.Pacote;
+import turismo.models.Pessoas.Cliente;
+
+public class Reserva<Reservavel> {
+    private Reservavel reserva;
+    private Cliente cliente;
+    private LocalDate dataReserva;
+    private 
+}
