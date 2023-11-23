32d7714c139d582c0589df11f1eccc3003b4f95c
Ka1ine <yasminkaline73@gmail.com>
2023-11-22 22:59:02 -0300

    Gerenciamento de Clientes

--- src/turismo/controllers/ClienteControllerImpl.java
+++ src/turismo/controllers/ClienteControllerImpl.java
@@ -20,4 +20,34 @@ public class ClienteControllerImpl implements ClienteController {
         // Lógica de busca
         return null;
     }
+
+    @Override
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    @Override
+    public Cliente obterClientePorId(long id) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getId() == id) {
+                return cliente;
+            }
+        }
+        return null; 
+    }
+
+    @Override
+    public void atualizarCliente(Cliente cliente) {
+        int index = clientes.indexOf(cliente);
+        if (index != -1) {
+            clientes.set(index, cliente);
+        }
+        
+    }
+
+    @Override
+    public void removerCliente(Cliente cliente) {
+        clientes.remove(cliente);  
+    }
+    
 }
--- src/turismo/models/Pessoas/Gerente.java
+++ src/turismo/models/Pessoas/Gerente.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 
 //Além das funções de um funcionário normal também consegue cadastrar, remover e alterar pacotes
 public class Gerente extends Funcionario {
-    public Gerente(String nome, int documento, int id, int telefone, String email, LocalDate aniversario) {
+    public Gerente(String nome, long documento, long id, long telefone, String email, LocalDate aniversario) {
         super(nome, documento, id, telefone, email, aniversario);
     }
 }
